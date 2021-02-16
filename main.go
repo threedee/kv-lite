dfd6133094cb446870bfadef5f5b98ff5b89212e
Dennis Engelbrecht <dennis@engelbrecht.org>
2021-02-16 13:23:13 +0100

    some refactoring. adding cli flags

--- main.go
+++ main.go
@@ -2,24 +2,24 @@ package main
 
 import (
 	"encoding/json"
+	"flag"
+	"fmt"
 	"io/ioutil"
 	"log"
 	"net/http"
-	"os"
 	"sync"
 )
 
-// RWMap uses a lock for map
-type RWMap struct {
-	sync.RWMutex
-	m map[string][]byte
+// KVStore uses a lock for map
+type KVStore struct {
+	mu       sync.RWMutex
+	m        map[string][]byte
+	fileName string
 }
 
 // Get locks map and retrieves value from map
-func (r *RWMap) Get(w http.ResponseWriter, key string) {
-	r.RLock()
-	defer r.RUnlock()
-	if value := r.m[key]; len(value) != 0 {
+func (k *KVStore) Get(w http.ResponseWriter, key string) {
+	if value := k.m[key]; len(value) != 0 {
 		w.Write(value)
 	} else {
 		w.WriteHeader(http.StatusNotFound)
@@ -27,57 +27,47 @@ func (r *RWMap) Get(w http.ResponseWriter, key string) {
 }
 
 // Set the value for given key with locking map
-func (r *RWMap) Set(w http.ResponseWriter, key string, value []byte) {
-	r.Lock()
-	defer r.Unlock()
-	r.m[key] = value
+func (k *KVStore) Set(w http.ResponseWriter, key string, value []byte) {
+	k.m[key] = value
 	w.WriteHeader(http.StatusNoContent)
-	r.save()
+	k.save()
 }
 
 // Delete key from map with Lock
-func (r *RWMap) Delete(w http.ResponseWriter, key string) {
-	r.Lock()
-	defer r.Unlock()
-	delete(r.m, key)
+func (k *KVStore) Delete(w http.ResponseWriter, key string) {
+	delete(k.m, key)
 	w.WriteHeader(http.StatusNoContent)
-	r.save()
+	k.save()
 }
 
-func (r *RWMap) save() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE|os.O_TRUNC, 0666)
+func (k *KVStore) save() error {
+	k.mu.Lock()
+	defer k.mu.Unlock()
 
-	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
-	}
-	defer db.Close()
-	json.NewEncoder(db).Encode(r.m)
-}
-
-func (r *RWMap) init() {
-	db, err := os.OpenFile(dbFileName, os.O_RDWR|os.O_CREATE, 0666)
-
-	if err != nil {
-		log.Fatalf("problem opening %s %v", dbFileName, err)
-	}
-	defer db.Close()
-
-	info, err := db.Stat()
-
-	if err != nil {
-		log.Fatalf("problem getting file info from file %s, %v", db.Name(), err)
-	}
-
-	if info.Size() != 0 {
-		if err = json.NewDecoder(db).Decode(&r.m); err != nil {
-			log.Fatalf("problem parsing map, %v", err)
+	var err error
+	if file, err := json.Marshal(k.m); err == nil {
+		if err = ioutil.WriteFile(k.fileName, file, 0644); err == nil {
+			return nil
 		}
-	} else {
-		r.m = make(map[string][]byte)
 	}
+	return fmt.Errorf("problem saving file %s, %v", k.fileName, err)
+
 }
 
-func (r *RWMap) ServeHTTP(w http.ResponseWriter, req *http.Request) {
+func (k *KVStore) init() {
+	k.mu.Lock()
+	defer k.mu.Unlock()
+
+	if file, err := ioutil.ReadFile(k.fileName); err == nil {
+		if err = json.Unmarshal(file, &k.m); err == nil {
+			return
+		}
+	}
+
+	k.m = make(map[string][]byte)
+}
+
+func (k *KVStore) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 	key := req.URL.Path[len("/"):]
 
 	switch req.Method {
@@ -87,24 +77,28 @@ func (r *RWMap) ServeHTTP(w http.ResponseWriter, req *http.Request) {
 		if err != nil {
 			log.Fatalf("error reading body, %v", err)
 		}
-		r.Set(w, key, body)
+		k.Set(w, key, body)
 	case http.MethodGet:
-		r.Get(w, key)
+		k.Get(w, key)
 	case http.MethodDelete:
-		r.Delete(w, key)
+		k.Delete(w, key)
 	}
 }
 
-const dbFileName = "key_value.db.json"
-
 func main() {
-	store := &RWMap{}
+	filename := flag.String("f", "key_value.db.json", "file name used for the store.")
+	port := flag.Uint("p", 5000, "Port for the server to listen on")
+
+	flag.Parse()
+	store := &KVStore{}
+	store.fileName = *filename
+
 	store.init()
 
 	mux := http.NewServeMux()
 	mux.Handle("/", store)
 
-	if err := http.ListenAndServe(":5000", mux); err != nil {
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", *port), mux); err != nil {
 		log.Fatalf("could not listen on port 5000 %v", err)
 	}
 }
